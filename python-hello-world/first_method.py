c1219f8586a3677269ecd8278baf484eb7d434df
basakil <basakil@users.noreply.github.com>
2021-07-19 22:10:21 +0300

    first commit.

---
+++ python-hello-world/first_method.py
@@ -0,0 +1,21 @@
+# print("Hello World")
+
+
+def print_hello_world_twice():
+    print('hw')
+print("Hello World 1")
+print("Hello World 2")
+
+
+def print_hello_world_multiple_times(times):
+    for i in range(1, times+1):
+        print("Hello World")
+
+
+for i in range(1, 10): print(i); print('..')
+
+# print("Hello World 3")
+
+print_hello_world_twice()
+
+# print_hello_world_multiple_times(4)
